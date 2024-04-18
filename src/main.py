a50474afaa0c13b63e144bf05738d49afcf2b599
mmackowsky <maciostah@gmail.com>
2024-04-18 22:18:53 +0200

    feat(main): add new route

--- src/main.py
+++ src/main.py
@@ -11,8 +11,17 @@ db = SessionLocal()
 
 
 @app.get("/app/water", status_code=status.HTTP_200_OK)
-async def get_water_consumption(request: Request):
+async def get_water_consumptions(request: Request):
     return db.query(WaterConsumption).all()
+
+
+@app.get("/app/water/{water_consumption_id}", status_code=status.HTTP_200_OK)
+async def get_water_consumption_by_id(request: Request, water_consumption_id: int):
+    return (
+        db.query(WaterConsumption)
+        .filter(WaterConsumption.id == water_consumption_id)
+        .first()
+    )
 
 
 if __name__ == "__main__":
