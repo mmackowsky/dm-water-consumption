36dbf787076f93fc1e5d96b213f7711e31f8fe6e
mmackowsky <maciostah@gmail.com>
2024-05-08 18:41:04 +0200

    feat: add new celery tasks

--- src/worker.py
+++ src/worker.py
@@ -1,6 +1,6 @@
 import random
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from celery import Celery, shared_task
 
@@ -25,3 +25,29 @@ app.autodiscover_tasks()
 def create_task(task_type):
     time.sleep(int(task_type) * 10)
     return True
+
+
+@shared_task(name="add_water_consumption")
+def add_water_consumption(user_id):
+    session = SessionLocal()
+    water_consumption = random.randint(100, 1000)
+    measurement_date = datetime.now().strftime("%Y-%m-%d")
+    water = WaterConsumption(
+        user=user_id,
+        measurement_date=measurement_date,
+        water_consumption=water_consumption,
+    )
+    session.add(water)
+    session.commit()
+    session.refresh(water)
+    return water
+
+
+@app.task(name="periodic_task", bind=True, ignore_result=True)
+def periodic_task(self, user_id):
+    """
+    Test task set to 10 sec.
+    :return:
+    """
+    task = add_water_consumption.apply_async(countdown=10, retry=False, args=[user_id])
+    return f"Periodic task scheduled {task}"
