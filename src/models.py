911f5fc7c2ec7c5711746c2d242df0bca45437ee
mmackowsky <maciostah@gmail.com>
2024-05-21 21:02:36 +0200

    feat(models): update

--- src/models.py
+++ src/models.py
@@ -5,8 +5,14 @@ from database import Base
 
 class WaterConsumption(Base):
     __tablename__ = "water_consumption"
+    __table_args__ = {"extend_existing": True}
 
     id = Column(Integer, primary_key=True, nullable=False)
     user = Column(Integer, nullable=False)
     measurement_date = Column(String, nullable=False)
     water_consumption = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return (
+            f"{self.id}, {self.user}, {self.measurement_date}, {self.water_consumption}"
+        )
