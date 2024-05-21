519db3b3e8d84e95522c7966097e9ec5f5e9a389
mmackowsky <maciostah@gmail.com>
2024-05-21 20:05:05 +0200

    tests: add tests

---
+++ src/tests.py
@@ -0,0 +1,46 @@
+import unittest
+from datetime import datetime
+
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.pool import StaticPool
+
+from database import Base, get_db
+from main import app
+from src.models import WaterConsumption
+
+SQLALCHEMY_DATABASE_URL = "sqlite://"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+db = TestingSessionLocal()
+energy = WaterConsumption(
+    id=1,
+    user=1,
+    measurement_date=datetime.now().strftime("%Y-%m-%d"),
+    water_consumption=100,
+)
+db.add(energy)
+db.commit()
+db.refresh(energy)
